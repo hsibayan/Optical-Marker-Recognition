5ea050f7fb369652b6e820234e26b2ed9ca66544
jetforce <jetvirtusio@hotmail.com>
2016-11-11 02:35:45 +0800

    Dis is da latest change

---
+++ normalize/fourpoint.py
@@ -0,0 +1,72 @@
+import numpy as np
+import cv2
+
+
+
+def order_points(pts):
+	# initialzie a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype = "float32")
+ 
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+ 
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+ 
+	print ("this is rect")
+	print (rect)
+	print ("this is one point")
+	print (rect[0])
+	print (rect[0][0])
+
+	width =  rect[1][0] - rect[0][0]
+	height = rect[3][1] - rect[0][1]
+	print (width)
+	print (height)
+
+	if(width > height):
+		rect2 = np.zeros((4, 2), dtype = "float32")
+		rect2[0] = rect[3] 
+		rect2[1] = rect[0]
+		rect2[2] = rect[1]
+		rect2[3] = rect[2]
+		return rect2 
+
+	# return the ordered coordinates
+	return rect
+
+
+def four_point_transform(image, pts):
+
+
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+ 
+
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	return warped
---
+++ normalize/normalize.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+import fourpoint as f
+import rotate
+import imutils
+
+
+image = cv2.imread("rotate4.jpg")
+
+height = np.size(image, 0)
+width = np.size(image, 1)
+
+if(height < width):
+	image = imutils.resize(image.copy(), height=750)
+else:
+	image = imutils.resize(image.copy(), width=750)
+
+# cv2.imshow("original",image)
+# cv2.imshow("normal,",normal_image)
+
+scratch = image.copy()
+gray = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray.copy(), (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+#opening = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+
+im2, cnts, hier = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+sorted_c = sorted(cnts, key = cv2.contourArea, reverse = True)
+
+
+for c in sorted_c:
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+	if len(approx) == 4:
+		paper = approx
+		break
+
+
+
+cv2.drawContours(scratch, [paper], -1, (0,255,0), 3)
+warped = f.four_point_transform(image.copy(), paper.reshape(4, 2))
+warped = cv2.resize(warped, (1275, 1650)) 
+cv2.imshow("Warped",warped)
+cv2.imshow("Original",image)
+cv2.imshow("Gray",gray)
+cv2.imshow("Edges",edged)
+cv2.imshow("Scratch",scratch)
+final = rotate.fixRotation(warped)
+
+cv2.imwrite("output.png",final)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
